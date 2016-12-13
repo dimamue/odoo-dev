9195fcc8071deb12adaa7d525a5ee8a79e79fb6e
dimamue <dimitri.mueller1991@gmail.com>
2016-12-13 12:47:07 +0100

    initial projekt version

---
+++ custom-addons/todo_app/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import odoo
+from odoo import models, fields, api
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
